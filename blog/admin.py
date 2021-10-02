5db7a243cd5348e5ccd57ba39362fbce07d9eca8
Mostafa <aria.ra3@gmail.com>
2021-10-02 16:46:21 +0330

    make a buttom for pagination

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,20 @@
 from django.contrib import admin
-from .models  import Article
+from django.contrib.admin.sites import AdminSite
+from .models import Article,Category
 
 # Register your models here.
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title','author','jpublish','jcreated','status')
-    list_filter = ('author','publish','status','created')
-    search_fields = ('author','publish','status')
-    prepopulated_fields={'slug':('title'[:10],)}
 
-admin.site.register(Article,ArticleAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'jpublish', 'status')
+    list_filter = ('author', 'publish', 'status')
+    search_fields = ('author', 'publish', 'status')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'position', 'publish')
+
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,32 +2,47 @@ from django.db import models
 from django.utils import timezone
 from extensions.utils import jalali_converter
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name="نام دسته بندی")
+    position = models.IntegerField(verbose_name="پوزیشن")
+    publish = models.BooleanField(verbose_name="منتشر شود؟")
+    
+    class Meta:
+        ordering = ['position']
+
+
 class Article(models.Model):
     STATUS_CHOICES = (
-        ('d','پیشنویس'),
-        ('p','منتشر شده'),
+        ('d', 'پیشنویس'),
+        ('p', 'منتشر شده'),
     )
-    author = models.CharField(max_length=50 , verbose_name="گردآوری شده توسط")
-    title = models.CharField(max_length=100,verbose_name="عنوان مقاله")
-    slug = models.SlugField(max_length=100,unique=True,verbose_name="آدرس مقاله")
+    author = models.CharField(max_length=50, verbose_name="گردآوری شده توسط")
+    title = models.CharField(max_length=100, verbose_name="عنوان مقاله")
+    slug = models.SlugField(max_length=100, unique=True,
+                            verbose_name="آدرس مقاله")
     description = models.TextField(verbose_name="محتوا")
-    thumbnail=models.ImageField(upload_to="images",verbose_name="تصویر")
-    publish = models.DateTimeField(default=timezone.now,verbose_name="زمان انتشار")
-    created = models.DateTimeField(auto_now_add=True,verbose_name="زمان تولید مقاله")
+    thumbnail = models.ImageField(upload_to="images", verbose_name="تصویر")
+    publish = models.DateTimeField(default=timezone.now,
+                                   verbose_name="زمان انتشار")
+    category = models.ManyToManyField(Category)
     updated = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=1,choices=STATUS_CHOICES,verbose_name="وضعیت")
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES,
+                              verbose_name="وضعیت")
+    
     def __str__(self):
         return self.title
+    
     class Meta:
         verbose_name = "مقاله"
         verbose_name_plural = "مقالات"
-        ordering = ['-publish','status','created']
+        ordering = ['-publish', 'status']
     
-    def jpublish (self):
+    def jpublish(self):
         return jalali_converter(self.publish)
-    jpublish.short_description= "زمان انتشار"
+    jpublish.short_description = "زمان انتشار"
     
-    def jcreated (self):
+    def jcreated(self):
         return jalali_converter(self.created)
     jcreated.short_description = "زمان ساخته شدن مقاله"
-    

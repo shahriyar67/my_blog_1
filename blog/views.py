8867e879d2fdc23b19af440b08d34d082638dacc
Mostafa <aria.ra3@gmail.com>
2021-10-06 01:35:54 +0330

    some styles added

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,19 @@
-from django.shortcuts import render,get_object_or_404
-from .models import Article
+from django.shortcuts import render, get_object_or_404
+from .models import Article, Category
 
 # Create your views here.
 
+
 def home(request):
     context = {
-        "article":Article.objects.filter(status="p").order_by('-publish')
+        "article": Article.objects.filter(status="p").order_by('-publish'),
+        "Category": Category.objects.filter(status=True)
     }
-    return render(request,'blog/home.html',context)
+    return render(request, 'blog/home.html', context)
 
-def detail(request,slug):
+
+def detail(request, slug):
     context = {
-        "article":get_object_or_404(Article,slug = slug, status ="p")
+        "article": get_object_or_404(Article, slug=slug, status="p")
     }
-    return render(request,"blog/detail.html",context)
+    return render(request, "blog/detail.html", context)

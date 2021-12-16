c3bdfba64020745ce9a6675c73cfc6699b5f709d
Mostafa <aria.ra3@gmail.com>
2021-12-16 12:33:06 +0330

    category template must bi fixed

--- blog/admin.py
+++ blog/admin.py
@@ -4,12 +4,53 @@ from .models import Article, Category
 
 # Register your models here.
 
+def make_published(modeladmin, request, queryset):
+    row_updated = queryset.update(status = 'p')
+    if row_updated == 1 :
+        massage_bit = "شد"
+    else:
+        massage_bit = "شدند"
+    modeladmin.message_user(request,"{} مقاله منتشر {}"
+                               .format(row_updated, massage_bit))
+make_published.short_description = "منتشر شدن"
+
+@admin.action(description='با دکوریتور پیش نویس شوند')
+def make_drafted(modeladmin, request, queryset):
+    row_updated = queryset.update(status = 'd')
+    if row_updated == 1 :
+        massage_bit = "شد"
+    else:
+        massage_bit = "شدند"
+    modeladmin.message_user(request,"{} مقاله پیش نویس {}"
+                               .format(row_updated, massage_bit))
+
+def status_True(modeladmin, request, queryset):
+    row_updated = queryset.update(status = True)
+    if row_updated == 1 :
+        massage_bit = "شد"
+    else:
+        massage_bit = "شدند"
+    modeladmin.message_user(request,"{} دسته بندی منتشر {}"
+                               .format(row_updated, massage_bit))
+status_True.short_description = "منتشر شدن"
+
+@admin.action(description='با دکوریتور پیش نویس شوند')
+def statue_Fales(modeladmin, request, queryset):
+    row_updated = queryset.update(status = False)
+    if row_updated == 1 :
+        massage_bit = "شد"
+    else:
+        massage_bit = "شدند"
+    modeladmin.message_user(request,"{} دسته بندی پیش نویس {}"
+                               .format(row_updated, massage_bit))
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('position', 'name', 'parent', 'slug', 'status')
     list_filter = (['status'])
     search_fields = ('name', 'slug')
     prepopulated_fields = {'slug': ('name',)}
+    actions = [statue_Fales, status_True]
     
 
 class ArticleAdmin(admin.ModelAdmin):
@@ -17,6 +58,7 @@ class ArticleAdmin(admin.ModelAdmin):
     list_filter = ('author', 'publish', 'status')
     search_fields = ('title', 'author', 'publish', 'status')
     prepopulated_fields = {'slug': ('title',)}
+    actions = [make_published, make_drafted]
     
     def category_to_str(self, obj):
         CatList = [i.name for i in obj.Category.published()]
--- blog/views.py
+++ blog/views.py
@@ -2,35 +2,47 @@ from django.core import paginator
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Article, Category
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class ArticleList(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 4
 
+#def detail(request, slug):
+#    context = {
+#        "article": get_object_or_404(Article, slug=slug, status="p"),
+#        
+#    }
+#    return render(request, "blog/detail.html", context)
 
-def home(request, page=1):
-    articles_list = Article.objects.published().order_by('-publish')
-    paginator = Paginator(articles_list, 4)
-    articles = paginator.get_page(page)
-    context = {
-        "article": articles,
-        }
-    return render(request, 'blog/home.html', context)
-
-
-def detail(request, slug):
-    context = {
-        "article": get_object_or_404(Article, slug=slug, status="p"),
-        
-    }
-    return render(request, "blog/detail.html", context)
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        return get_object_or_404(Article.objects.published(),slug= slug)
 
 
-def category(request, slug, page=1):
-    category = get_object_or_404(Category, slug=slug, status=True)
-    article_list = category.articles.published()
-    paginator = Paginator(article_list, 4)
-    articles = paginator.get_page(page)
-    context = {
-        "category": category,
-        "articles": articles
-    }
-    return render(request, 'blog/category.html', context)
+#def category(request, slug, page=1):
+#    category = get_object_or_404(Category, slug=slug, status=True)
+#    article_list = category.articles.published()
+#    paginator = Paginator(article_list, 4)
+#    articles = paginator.get_page(page)
+#    context = {
+#        "category": category,
+#        "articles": articles
+#    }
+#    return render(request, 'blog/category.html', context)
+#
+
+class CategoryList(ListView):
+    paginate_by = 5
+    def get_queryset(self):
+        slug = self.kwargs.get('slug')
+        global category
+        category = get_object_or_404(
+            Category.objects.published(),slug= slug)
+        return category.articles.published()
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = category
+        return context

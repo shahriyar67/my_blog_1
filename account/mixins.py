b6e20190fe3a4ce938e86cf5bcdb8fb7d5f1568f
Mostafa <aria.ra3@gmail.com>
2022-02-12 11:42:54 +0330

    jalase 54 anjam shod. fagat icon log out kar nemikonad!

--- account/mixins.py
+++ account/mixins.py
@@ -39,3 +39,11 @@ class AuthorAccessMixin():
           return super().dispatch(request, *args, **kwargs)
         else:
             raise Http404("YOU CANT SEE THIS PAGE!")
+
+
+class SuperUserAccessMixin():
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            raise Http404("YOU CANT SEE THIS PAGE!!")
--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from blog.models import Article
-from .mixins import FieldMixins, FormValidMixin, AuthorAccessMixin
+from django.urls import reverse_lazy
+from .mixins import FieldMixins, FormValidMixin, AuthorAccessMixin, SuperUserAccessMixin
 # Create your views here.
 
 class ArticleList(LoginRequiredMixin, ListView):
@@ -22,3 +23,9 @@ class ArticleCreate(LoginRequiredMixin, FormValidMixin, FieldMixins, CreateView)
 class ArticleUpdate(AuthorAccessMixin, FormValidMixin, FieldMixins, UpdateView):
     model = Article
     template_name = 'registration/Article_create_update.html'
+    
+
+class ArticleDelete(SuperUserAccessMixin, DeleteView):
+    model = Article
+    success_url = reverse_lazy('account:home')
+    template_name = 'registration/article_delete.html'
